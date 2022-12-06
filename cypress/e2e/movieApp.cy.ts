fe28afa7a9ff2d7ae0511893b2038fc96369c191
adamovd <adam.ov.danielsson@gmail.com>
2022-12-06 14:23:55 +0100

    added mock response in fixures

--- cypress/e2e/movieApp.cy.ts
+++ cypress/e2e/movieApp.cy.ts
@@ -1,4 +1,6 @@
-import { data } from "cypress/types/jquery";
+import { movies, response } from "./mock";
+
+movies;
 
 describe("testing movieapp", () => {
   it("should show form", () => {
@@ -12,25 +14,30 @@ describe("testing movieapp", () => {
   });
 
   it("should search with value", () => {
+    cy.intercept("GET", "http://omdbapi.com/*", { fixture: "mocks" }).as(
+      "movieSearch"
+    );
     cy.visit("http://localhost:1234");
-    cy.intercept("GET", "http://omdbapi.com/*", {}).as("movieSearch");
-    cy.get("input").type("Star").should("have.value", "Star");
+    cy.get("input").type("Star");
     cy.get("button").click();
     cy.wait("@movieSearch").its("request.url").should("contain", "Star");
   });
 
   it("should create divs", () => {
+    cy.intercept("GET", "http://omdbapi.com/*", { fixture: "mocks" }).as(
+      "movieSearch"
+    );
     cy.visit("http://localhost:1234");
-    cy.intercept("GET", "http://omdbapi.com/*").as("movieSearch");
     cy.get("input").type("Star").should("have.value", "Star");
     cy.get("button").click();
+    cy.get("div:first").should("have.id", "movie-container");
     cy.get(".movie").should("have.length", 10);
     cy.get(".movie:first").contains("A New Hope");
   });
 
   it("should show noMessage", () => {
     cy.visit("http://localhost:1234");
-    cy.get("input").type("s").should("have.value", "s");
+    cy.get("input").type("s");
     cy.get("button").click();
     cy.get("p").contains("sökresultat");
   });
